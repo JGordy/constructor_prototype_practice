0d7efee503f7142e606284a107c2ad752e773391
Joseph Gordy <jgordy2424@gmail.com>
2017-09-11 11:33:11 -0400

    Completed all tests with PASSED status using constructors and prototype

--- starter_files/main.js
+++ starter_files/main.js
@@ -25,15 +25,18 @@ let atticus = new Dog("normal", "brown", false);
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human (dog) {
-
+function Human (cool) {
+  this.cool = cool;
 }
 
 Human.prototype.pet = function(dog) {
   dog.status = "happy";
 }
+Human.prototype.feed = function(dog) {
+  dog.hungry = false;
+}
 
 // Instances of Human
 // Needed: mason, julia
-let mason = new Human();
-let julia = new Human();
+let mason = new Human(false);
+let julia = new Human(true);
