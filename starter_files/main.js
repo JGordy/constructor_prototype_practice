570a334e1f0b69151356d42dcdc4523008a74dd2
Joseph Gordy <jgordy2424@gmail.com>
2017-09-11 11:29:27 -0400

    Used constructors and prototype to pass the first five questions

--- starter_files/main.js
+++ starter_files/main.js
@@ -7,11 +7,17 @@
 //            /____/
 
 // Dog Constructor & Prototype
-function Dog () {}
+function Dog (status, color, hungry) {
+  this.status = "normal";
+  this.color = color;
+  this.hungry = hungry;
+}
 
 // Instances of Dog
 // Needed: sadie, moonshine, atticus
-
+let sadie = new Dog("normal", "black", false);
+let moonshine = new Dog("normal", "white", true);
+let atticus = new Dog("normal", "brown", false);
 //     __
 //    / /_  __  ______ ___  ____ _____  _____
 //   / __ \/ / / / __ `__ \/ __ `/ __ \/ ___/
@@ -19,7 +25,15 @@ function Dog () {}
 // /_/ /_/\__,_/_/ /_/ /_/\__,_/_/ /_/____/
 
 // Human Constructor & Prototype
-function Human () {}
+function Human (dog) {
+
+}
+
+Human.prototype.pet = function(dog) {
+  dog.status = "happy";
+}
 
 // Instances of Human
 // Needed: mason, julia
+let mason = new Human();
+let julia = new Human();
